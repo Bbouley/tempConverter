aba4feba8f707be9f40e442d1d893f000d849b5b
bbouley <b.bouley@hotmail.co.uk>
2015-07-12 13:36:03 -0600

    edited to allow user to choose C or F

--- tempConverter.js
+++ tempConverter.js
@@ -1,5 +1,20 @@
-var tempC = prompt('What temperature would you like to convert?');
+var fc = prompt('Do you want to convert from C or F?');
 
-var tempF = tempC * (9/5) + 32
+    while (fc != 'c' && fc!= 'C' && fc !='f' && fc !='F'){
+      fc = prompt('You have to choose C or F');
+    }
 
-alert(tempF)
+var temp = prompt('What\'s the temperature you\'d like to convert?')
+
+    while (temp === NaN){
+      temp = prompt('You have to choose a number bro!');
+    }
+
+
+    if (temp != NaN && fc === ('c'||'C') ){
+         var tf = temp * (9/5) + 32
+         alert('It is ' + tf + ' degrees Fahrenheit')
+    } else if (temp != NaN && fc === ('f' || 'F')) {
+        var tc = (temp-32)*(5/9)
+        alert('It is ' + tc + ' degrees Celcius')
+    }
