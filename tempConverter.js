260d232a0c0758c1909b922b865febf70b2492b8
bbouley <b.bouley@hotmail.co.uk>
2015-07-12 13:37:45 -0600

    changed answers to only go to 2 decimal places

--- tempConverter.js
+++ tempConverter.js
@@ -12,9 +12,11 @@ var temp = prompt('What\'s the temperature you\'d like to convert?')
 
 
     if (temp != NaN && fc === ('c'||'C') ){
-         var tf = temp * (9/5) + 32
+         var tfx = temp * (9/5) + 32
+         var tf = tfx.toFixed(2)
          alert('It is ' + tf + ' degrees Fahrenheit')
     } else if (temp != NaN && fc === ('f' || 'F')) {
-        var tc = (temp-32)*(5/9)
+        var tcx = (temp-32)*(5/9);
+        var tc = tcx.toFixed(2);
         alert('It is ' + tc + ' degrees Celcius')
     }
